c8da0dc72c6a81d9efc6bc5dffcc3948489dca2a
amirhamza05 <sk.amirhamza@gmail.com>
2021-11-09 04:53:49 +0600

    Update Profile Using NPM

---
+++ webpack.mix.js
@@ -0,0 +1,14 @@
+const mix = require('laravel-mix');
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resource/js/app.js', 'asset/js/script.js')
+    .sass('resource/sass/app.scss', 'asset/css/style.css');
